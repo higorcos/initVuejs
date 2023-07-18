c1479dbfac2196b20d9eed1bd9e6dcecf8fa531b
Hígor Pinheiro Costa <higorcosta2001@outlook.com>
2023-07-18 11:17:25 -0300

    Variaveis

---
+++ src/components/ProdutoCrud.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h3>Meu produto: {{ nameProduto }}</h3>
+        <input type="name" 
+        name="nomeProduto"
+        v-model="nameProduto" 
+        >
+        <p>Descrição do produto</p>
+        <p>{{ descricao }}</p>
+        <input type="txt" 
+        name="descricao"
+        v-model="descricao" 
+        >
+        <!-- :value="nameProduto"  == Variável reativa -->
+        {{ count }}
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      count: 0,
+      nameProduto: 'PC ',
+      descricao: 'Descri',
+     }
+    },
+    props: {
+        // nome: String,
+        // descricao: String,
+    },
+}
+</script>
+
+<style>
+
+</style>
